d4be8915e6a18bd15bb1b5e32e7d15a26444b239
foxtrotfire <foxtrot661@gmail.com>
2013-01-31 21:49:28 +0100

    Update chainloader.js

--- chainloader.js
+++ chainloader.js
@@ -1,12 +1,1 @@
-/*
- * chainloader.js - a loader for foxbot
- * 
- * Copyright (C) 2012 by 1NT and FoxtrotFire
- */
- 
-document.addEventListener('DOMContentLoaded',function(){
-	var scriptTag = document.createElement('script');
-	scriptTag.type = 'text/javascript';
-	scriptTag.src = 'https://raw.github.com/ssitaru/foxbot/master/foxbot.js';
-	document.body.appendChild(scriptTag);
-});
+rehosted
